ebb273211e24c8d7df1f0ae43ce8ce897657b132
paulawaster <pbukowska16@gmail.com>
2018-03-28 13:23:09 +0200

    zadanie 22.3*

--- src/test/java/com/kodilla/jdbc/StoredPostTestSuite.java
+++ src/test/java/com/kodilla/jdbc/StoredPostTestSuite.java
@@ -50,4 +50,37 @@ public class StoredPostTestSuite {
         }
         assertEquals(0, howMany);
     }
+
+    @Test
+    public void testUpdateBestsellerMoreCases() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER = false";
+        String sqlInsert = "INSERT INTO RENTS(BOOK_ID, READER_ID, RENT_DATE, RETURN_DATE) VALUES (1, 1, '2005-03-02', null)";
+
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        statement.executeUpdate(sqlInsert);
+
+        //When
+        String sqlProcedureCall = "CALL UpdateBestseller()";
+        statement.execute(sqlProcedureCall);
+
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER = false";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if(rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+
+        try {
+            assertEquals(1, howMany);
+        }
+        finally {
+            String sqlDel = "DELETE FROM RENTS WHERE RENT_DATE = '2005-03-02'";
+            Statement s = dbManager.getConnection().createStatement();
+            s.executeUpdate(sqlDel);
+        }
+    }
 }
